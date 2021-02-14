071bac2eebb30a2616c85835f8a9b2051d5db108
mat <27899617+mat-1@users.noreply.github.com>
2021-02-14 15:40:54 -0600

    create folders and github stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# skyblock.matdoes.dev
+
+Powered by [skyblock-api](https://github.com/skyblockstats/skyblock-api)
---
+++ build/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const nunjucks = __importStar(require("nunjucks"));
+const express = __importStar(require("express"));
+const app = express();
+const env = nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.get('/', function (req, res) {
+    res.render('index.html', {});
+});
+app.use('/', express.static('public'));
+app.listen(8081, () => console.log('pog'));
