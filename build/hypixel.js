5ab021536756a54c016dd0bd13e4699f234b2181
mat <27899617+mat-1@users.noreply.github.com>
2021-04-19 19:39:53 -0500

    Inventories & armor (#5)
    * start working on inventories
    
    * inventory items (somewhat) work now! :tada:
    
    * Compiled TS into JS
    
    * add heads to inventories
    
    * Compiled TS into JS
    
    * don't show ender chest if they don't have inventory api on
    
    * show all inventories with buttons to change which one is shown
    
    * Compiled TS into JS
    
    * inventory hotbar is at the bottom
    
    * add item tooltips
    
    * allow "locking" item tooltip by clicking on it
    
    * fix some wacky edge cases with tooltips
    
    * add armor display
    
    * improve skulls
    
    * Compiled TS into JS
    
    * fix armor being weird
    
    * bump skyblock-assets version
    
    * Compiled TS into JS

--- build/hypixel.js
+++ build/hypixel.js
@@ -1,12 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.INVENTORIES = exports.fetchLeaderboards = exports.fetchLeaderboard = exports.fetchProfile = exports.fetchPlayer = exports.baseApi = void 0;
+exports.INVENTORIES = exports.cacheInventories = exports.itemToUrlCached = exports.itemToUrl = exports.fetchLeaderboards = exports.fetchLeaderboard = exports.fetchProfile = exports.fetchPlayer = exports.baseApi = void 0;
 const node_fetch_1 = __importDefault(require("node-fetch"));
+const node_cache_1 = __importDefault(require("node-cache"));
 const https_1 = require("https");
 // import { Agent } from 'http'
+const skyblockAssets = __importStar(require("skyblock-assets"));
 if (!process.env.key)
     // if there's no key in env, run dotenv
     require('dotenv').config();
@@ -55,6 +76,56 @@ async function fetchLeaderboards() {
     return await fetchApi(`leaderboards`);
 }
 exports.fetchLeaderboards = fetchLeaderboards;
+const itemToUrlCache = new node_cache_1.default({
+    stdTTL: 60,
+    checkperiod: 5,
+    useClones: false,
+});
+async function itemToUrl(item) {
+    const stringifiedItem = JSON.stringify(item);
+    if (itemToUrlCache.has(stringifiedItem))
+        return itemToUrlCache.get(stringifiedItem);
+    const itemNbt = {
+        display: {
+            Name: item.display.name
+        },
+        ExtraAttributes: {
+            id: item.id,
+        }
+    };
+    let textureUrl;
+    if (item.head_texture)
+        textureUrl = `https://mc-heads.net/head/${item.head_texture}`;
+    else
+        textureUrl = await skyblockAssets.getTextureUrl({
+            id: item.vanillaId,
+            nbt: itemNbt,
+            pack: 'packshq'
+        });
+    if (!textureUrl) {
+        console.log(item);
+    }
+    itemToUrlCache.set(stringifiedItem, textureUrl);
+    return textureUrl;
+}
+exports.itemToUrl = itemToUrl;
+function itemToUrlCached(item) {
+    if (!item)
+        return null;
+    const stringifiedItem = JSON.stringify(item);
+    return itemToUrlCache.get(stringifiedItem);
+}
+exports.itemToUrlCached = itemToUrlCached;
+/** Get all the items in an inventories object to cache them */
+async function cacheInventories(inventories) {
+    const promises = [];
+    for (const inventoryItems of Object.values(inventories !== null && inventories !== void 0 ? inventories : {}))
+        for (const inventoryItem of inventoryItems)
+            if (inventoryItem)
+                promises.push(itemToUrl(inventoryItem));
+    await Promise.all(promises);
+}
+exports.cacheInventories = cacheInventories;
 exports.INVENTORIES = {
     armor: 'inv_armor',
     inventory: 'inv_contents',
---
+++ src/util.ts
@@ -0,0 +1,125 @@
+const colorCodes: { [ key: string ]: string } = {
+	'0': '#000000',
+	'1': '#0000be',
+	'2': '#00be00',
+	'3': '#00bebe',
+	'4': '#be0000', // red
+	'5': '#be00be',
+	'6': '#ffaa00', // gold
+	'7': '#bebebe',
+	'8': '#3f3f3f',
+	'9': '#3f3ffe',
+	'a': '#3ffe3f',
+	'b': '#3ffefe',
+	'c': '#fe3f3f', // light red
+	'd': '#fe3ffe',
+	'e': '#fefe3f',
+	'f': '#ffffff',
+}
+const specialCodes: { [ key: string ]: string } = {
+	'l': 'font-weight: bold'
+}
+
+const colorCodeCharacter = '§'
+
+export function formattingCodeToHtml(formatted: string): string {
+	let htmlOutput: string = ''
+	// we store the hex code, not the formatting code
+	let currentColor = null
+	// we store the css code, not the formatting code
+	let activeSpecialCodes: string[] = []
+
+	function reset() {
+		if (currentColor) {
+			htmlOutput += '</span>'
+			currentColor = null
+		}
+		while (activeSpecialCodes.pop()) {
+			htmlOutput += '</span>'
+		}
+	}
+
+	while (formatted.length > 0) {
+		const character = formatted[0]
+		formatted = formatted.slice(1)
+
+		// if it encounters § (or whatever colorCodeCharacter is), then read the next character
+		if (character === colorCodeCharacter) {
+			const colorCharacter = formatted[0]
+			formatted = formatted.slice(1)
+			if (colorCodes[colorCharacter]) {
+				if (currentColor !== colorCodes[colorCharacter]) { // make sure the color is different than the active one
+					// if there's already a color, close that tag
+					if (currentColor) htmlOutput += '</span>'
+					currentColor = colorCodes[colorCharacter]
+					htmlOutput += `<span style="color: ${currentColor}">`
+				}
+			} else if (specialCodes[colorCharacter]) {
+				if (!activeSpecialCodes.includes(specialCodes[colorCharacter])) {
+					activeSpecialCodes.push(specialCodes[colorCharacter])
+					htmlOutput += `<span style="${specialCodes[colorCharacter]}">`
+				}
+			} else if (colorCharacter === 'r') {
+				reset()
+			}
+		} else {
+			htmlOutput += character
+		}
+	}
+	reset()
+	return htmlOutput
+}
+
+
+
+function moveStringToEnd(word: string, thing: string) {
+	if (thing.startsWith(`${word}_`))
+		thing = thing.substr(`${word}_`.length) + `_${word}`
+	return thing
+}
+
+
+function millisecondsToTime(totalMilliseconds: number) {
+	const totalSeconds = totalMilliseconds / 1000
+	const totalMinutes = totalSeconds / 60
+	const totalHours = totalMinutes / 60
+
+	const milliseconds = Math.floor(totalMilliseconds) % 1000
+	const seconds = Math.floor(totalSeconds) % 60
+	const minutes = Math.floor(totalMinutes) % 60
+	const hours = Math.floor(totalHours)
+
+	const stringUnits: string[] = []
+
+	if (hours > 1) stringUnits.push(`${hours} hours`)
+	else if (hours == 1) stringUnits.push(`${hours} hour`)
+	if (minutes > 1) stringUnits.push(`${minutes} minutes`)
+	else if (minutes == 1) stringUnits.push(`${minutes} minute`)
+	if (seconds > 1) stringUnits.push(`${seconds} seconds`)
+	else if (seconds == 1) stringUnits.push(`${seconds} second`)
+	if (milliseconds > 1) stringUnits.push(`${milliseconds} milliseconds`)
+	else if (milliseconds == 1) stringUnits.push(`${milliseconds} millisecond`)
+	return stringUnits.slice(0, 2).join(' and ')
+}
+
+export function cleanNumber(number: number, unit?: string): string {
+	switch (unit) {
+		case 'time':
+			return millisecondsToTime(number)
+		case 'date':
+			return (new Date(number * 1000)).toUTCString()
+	}
+	return number.toLocaleString() + ' ' + unit
+}
+
+export function clean(thing: string | number) {
+	if (typeof thing === 'number') {
+		return cleanNumber(thing)
+	} else {
+		for (const string of ['deaths', 'kills', 'collection', 'skill'])
+			thing = moveStringToEnd(string, thing)
+		return thing
+			.replace(/^./, thing[0].toUpperCase())
+			.replace(/_/g, ' ')
+	}
+}
