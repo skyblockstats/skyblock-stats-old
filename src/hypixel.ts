c34abee32f12971bccf6bf38013ae5761e5f0f93
mat <github@matdoes.dev>
2021-08-10 15:34:50 -0500

    add some missing collection textures

--- src/hypixel.ts
+++ src/hypixel.ts
@@ -155,7 +155,7 @@ export async function itemToUrl(item: Item, packName?: string): Promise<string> 
 		textureUrl = await skyblockAssets.getTextureUrl({
 			id: item.vanillaId,
 			nbt: itemNbt,
-			pack: packName || 'packshq'
+			pack: packName ?? 'packshq'
 		})
 	
 	if (!textureUrl) {
@@ -197,12 +197,18 @@ const skyblockItems: { [ itemName: string ]: Item } = {
 	acacia_log: { vanillaId: 'minecraft:log2' },
 	birch_log: { vanillaId: 'minecraft:log:2' },
 	cod: { vanillaId: 'minecraft:fish' },
-	dark_oak_log: { vanillaId: 'minecraft:log:2' },
+	dark_oak_log: { vanillaId: 'minecraft:log2:1' },
 	jungle_log: { vanillaId: 'minecraft:log:3' },
 	oak_log: { vanillaId: 'minecraft:log' },
 	pufferfish: { vanillaId: 'minecraft:fish:3' },
 	salmon: { vanillaId: 'minecraft:fish:1' },
 	spruce_log: { vanillaId: 'minecraft:log:1' },
+	// hypixel named the collection "gemstone_collection" instead of "gemstone"
+	gemstone_collection: {
+		vanillaId: 'minecraft:skull',
+		head_texture: '39b6e047d3b2bca85e8cc49e5480f9774d8a0eafe6dfa9559530590283715142'
+	},
+	hard_stone: { vanillaId: 'minecraft:stone' },
 }
 
 
